5c1b0fdffdb507e23ce08bf48634ef65d7fb895d
youssef-md <emaildeyoussefmuhamad@gmail.com>
2019-10-02 03:39:31 -0300

    Update styleguides

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-const express = require("express");
+const express = require('express');
 
 const PORT = 3000;
 
-const HOST = "0.0.0.0";
+const HOST = '0.0.0.0';
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 
 console.log(`Acess in http://${HOST}:${PORT}`);
