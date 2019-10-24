291124b179ef29fa1d9cce847df707e8effda623
marcosnbj <marcosnery.comp@gmail.com>
2019-10-24 20:20:42 -0300

    pax-app/Wiki#174 Adding template for unprotected get route

--- src/routes.js
+++ src/routes.js
@@ -3,46 +3,57 @@ import { Router } from 'express';
 const routes = new Router();
 var request = require('request')
 
-function verifyUserStatus(req,res,next){
+function userAuthentication(req,res,next){
 
+  //Checking for the presence of an Authorization header
   if(!req.header('Authorization')){
     res.json({"Error":"Missing authorization token"});
   }
-  else{
-  var url = 'http://pax-user.herokuapp.com/auth/status'; //Configure environment variable for user service
-
-  //set header
+  
+  //User service URL at get user status route
+  var url = 'http://pax-user.herokuapp.com/auth/status';
+  
+  //Set header with authorization token received
   var headers = {
       'Authorization': String(req.header('Authorization'))
   };
   
-  request({headers: headers, url: url, method: 'GET'}, function (error, response, body) {
-     
+  request({headers: headers, url: url, method: 'GET'},  (error, response, body) => {
+    
+    if(error){
+      res.send(error)
+    }
+    
+    //If received response is not success, then send forbidden status
     if(JSON.parse(body)['message']!='success'){
       res.sendStatus(403);
     }
+    
+    //Else means authorized request, go to next step of the chain
     next(); 
 
   });
+
 }
-}
 
-//Protected route
-routes.get('/lel', verifyUserStatus, (req, res) => {
+
+//Unprotected route, no authentication step
+routes.get('/provider_by_category', (req, res) => {
   
-  res.send('Good');
-});
-
-
-//Unprotected route
-routes.get('/', (req, res) => {
+  //Passing category id to URL
+  var url = "http://pax-user.herokuapp.com/provider_by_category/"+req.query.id
   
+  //GET Request to User service
+  request({url: url, method: 'GET'}, (error, response, body) => {
+      
+      if(error){
+        res.send(error)
+      }
+    
+      res.json(JSON.parse(body))
+  });
 
-
-  res.send('Hello World :D');
 });
-
-
 
 
 export default routes;
