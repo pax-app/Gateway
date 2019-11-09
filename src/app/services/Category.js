76a01b071fb86dc21a3593ae081b396d03873423
Lucas Dutra <ldutra98@gmail.com>
2019-11-09 15:26:53 -0200

    pax-app/Wiki#194 Adapt request to async function
    Co-authored-by: Youssef Muhamad <emaildeyoussefmuhamad@gmail.com>

--- src/app/services/Category.js
+++ src/app/services/Category.js
@@ -1,16 +1,17 @@
 import axios from 'axios';
+import { baseGet } from '../utils/category';
 
-class Category {
-  async getGeneralCategories(req, res) {
-    const url = 'http://172.25.0.1:5002/category';
-    try {
-      const response = await axios.get(`${url}/general`);
-      return res.json(response.data);
-    } catch (error) {
-      return res.json({
-        status: 'error',
-      });
-    }
-  }
+const url = 'http://172.25.0.1:5002/category';
+
+export async function getGeneralCategories(req, res) {
+  res.json(await baseGet(`${url}/general`));
 }
-export default new Category();
+
+export async function getProviderCategories(req, res) {
+  res.json(await baseGet(`${url}/provider`));
+}
+
+export async function getProviderCategoriesFromGeneral(req, res) {
+  const { general_category_id } = req.params;
+  res.json(await baseGet(`${url}/provider/${general_category_id}`));
+}
