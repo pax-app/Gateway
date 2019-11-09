7809fea013c4bfaea048edcea27c23fa3c607c1f
Lucas Dutra <ldutra98@gmail.com>
2019-11-09 18:29:57 -0200

    pax-app/Wiki#194 Change util file name reference

--- src/app/utils/baseRequest.js
+++ src/app/utils/baseRequest.js
@@ -5,6 +5,7 @@ export async function baseGet(route) {
     const response = await axios.get(route);
     return response.data;
   } catch (error) {
+    console.log(error);
     return {
       status: 'error',
     };
