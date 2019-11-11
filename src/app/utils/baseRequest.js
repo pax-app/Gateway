bfbc321234398a1ee2e38b3b2b3c06c106ec2aa1
Lucas Dutra <ldutra98@gmail.com>
2019-11-10 23:09:44 -0300

    pax-app/Wiki#194 Create base post method

--- src/app/utils/baseRequest.js
+++ src/app/utils/baseRequest.js
@@ -5,7 +5,17 @@ export async function baseGet(route) {
     const response = await axios.get(route);
     return response.data;
   } catch (error) {
-    console.log(error);
+    return {
+      status: 'error',
+    };
+  }
+}
+
+export async function basePost(route, content) {
+  try {
+    const response = await axios.post(route, content);
+    return response.data;
+  } catch (error) {
     return {
       status: 'error',
     };
