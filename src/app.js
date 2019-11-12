5dd4cc3344a5e3ed367a7082bf334f523e6cdfff
youssef-md <emaildeyoussefmuhamad@gmail.com>
2019-11-12 16:58:47 -0300

    pax-app/Wiki#201 Integrate Gateway with Sentry to notify the team when something went wrong

--- src/app.js
+++ src/app.js
@@ -1,19 +1,28 @@
 import express from 'express';
+import 'express-async-errors';
+import * as Sentry from '@sentry/node';
 import routes from './routes';
 
 class App {
   constructor() {
     this.server = express();
+
+    Sentry.init({
+      dsn: 'https://4842a967766a49ec878d7cfccba14d60@sentry.io/1817642',
+    });
+
     this.middlewares();
     this.routes();
   }
 
   middlewares() {
+    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(express.json());
   }
 
   routes() {
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.errorHandler());
   }
 }
 
