6b0da92a7e085e960ac4e0b39687f476f5802224
youssef-md <emaildeyoussefmuhamad@gmail.com>
2019-11-12 17:15:19 -0300

    pax-app/Wiki#201 Add a global exception handler

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import 'express-async-errors';
+import Youch from 'youch';
 import * as Sentry from '@sentry/node';
+
 import routes from './routes';
 
 class App {
   constructor() {
     this.server = express();
+    this.node_env = process.env.NODE_ENV || 'development';
 
     Sentry.init({
       dsn: 'https://4842a967766a49ec878d7cfccba14d60@sentry.io/1817642',
@@ -13,6 +16,7 @@ class App {
 
     this.middlewares();
     this.routes();
+    this.exceptionHandler();
   }
 
   middlewares() {
@@ -23,6 +27,16 @@ class App {
   routes() {
     this.server.use(routes);
     this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      if (this.node_env === 'development') {
+        const errors = await new Youch(err, req).toJSON();
+        return res.status(500).json(errors);
+      }
+      return res.status(500).json({ error: 'Internal server error :/' });
+    });
   }
 }
 
